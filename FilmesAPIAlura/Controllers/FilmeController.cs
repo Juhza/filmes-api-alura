143ba1e46a2348aec162468b5d41a44b8757758b
Júlia Beatriz Adão <jubza@edu.univali.br>
2021-07-16 11:22:34 -0300

    adicionado método para recuperar filme por id

--- FilmesAPIAlura/Controllers/FilmeController.cs
+++ FilmesAPIAlura/Controllers/FilmeController.cs
@@ -12,13 +12,25 @@ namespace FilmesAPIAlura.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionarFilme([FromBody] Filme filme)
         {
-
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> BuscarFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme BuscarFilmePorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
