126b28a7a397786cbe1fd1972b8ff6133f66f8c0
Júlia Beatriz Adão <jubza@edu.univali.br>
2021-07-15 21:14:45 -0300

    annotations :)

---
+++ FilmesAPIAlura/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesAPIAlura.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPIAlura.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
